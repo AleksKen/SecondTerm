0edce5165929eec3e301c698cb875908495e5810
AleksKen <aleks.ken2017@mail.ru>
2022-03-24 15:59:09 +0300

    вторые правки

--- src/main/java/tasks/Library.java
+++ src/main/java/tasks/Library.java
@@ -1,6 +1,7 @@
 package tasks;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Хранит информацию о книгах и позволяет их искать.
@@ -14,8 +15,7 @@ import java.util.HashSet;
  */
 
 public class Library {
-
-    public HashSet<Book> library = new HashSet<Book>();
+    private final HashSet<Book> library = new HashSet<Book>();
 
     public static void main(String args[]) {
 
@@ -32,15 +32,12 @@ public class Library {
     }
 
     //удалить книгу
-    public void remove(Book oneBook) {
-        if (library.contains(oneBook))
-            library.remove(oneBook);
-        else
-            throw new NullPointerException("такой книги не существует");
+    public boolean remove(Book oneBook) {
+        return library.remove(oneBook);
     }
 
     //изменить параметр книги (вводить параметры, которые хотим изменить, остальные null)
-    public void changes(Book oldBook, String name, String author, String genre) {
+    public boolean changes(Book oldBook, String name, String author, String genre) {
         if (library.contains(oldBook)) {
             String useName = oldBook.getName();
             String useAuthor = oldBook.getAuthor();
@@ -54,31 +51,29 @@ public class Library {
             Book ans = new Book(useName, useAuthor, useGenre, oldBook.getCode());
             library.remove(oldBook);
             library.add(ans);
+            return true;
         } else
-            throw new NullPointerException("отсутствует книга для изменений");
+            return false;
     }
 
     //переместить книгу на другую полку
-    public void shift(Book oldBook, String newCode) {
+    public boolean shift(Book oldBook, String newCode) {
         Book makeBook = new Book(oldBook.getName(), oldBook.getAuthor(), oldBook.getGenre(), newCode);
-        if (library.contains(oldBook)) {
-            library.remove(oldBook);
+        if (library.remove(oldBook)) {
             library.add(makeBook);
+            return true;
         } else
-            throw new NullPointerException("книга для перемещения не найдена");
+            return false;
     }
 
     //поиск книги (вводить известные параметры, неизвестные null)
-    public HashSet<Book> search(String name, String author, String genre, String code) {
-        HashSet<Book> booksForYou = new HashSet<Book>();
+    public Set<Book> search(String name, String author, String genre, String code) {
+        Set<Book> booksForYou = new HashSet<Book>();
         for (Book i : library) {
             if (i.simile(name, author, genre, code))
                 booksForYou.add(i);
         }
-        if (booksForYou.isEmpty())
-            throw new NullPointerException("такой книги не существует");
-        else
-            return booksForYou;
+        return booksForYou;
     }
 }
 
--- src/test/java/tasks/LibraryTest.java
+++ src/test/java/tasks/LibraryTest.java
@@ -3,6 +3,7 @@ package tasks;
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -21,18 +22,13 @@ public class LibraryTest {
         Library library = new Library();
         library.add(new Book("гордость и предубеждение", "джейн остин", "роман", "A2"));
         library.add(new Book("мы", "замятин", "антиутопия", "Б3"));
-        library.remove(new Book("гордость и предубеждение", "джейн остин", "роман", "A2"));
+        assertTrue(library.remove(new Book("гордость и предубеждение", "джейн остин", "роман", "A2")));
         assertFalse(library.contains(new Book("гордость и предубеждение", "джейн остин", "роман", "A2")));
+
+        assertFalse(library.remove(new Book("гордость", "джейн остин", "роман", "A2")));
+        assertFalse(library.remove(new Book("vs", "замятин", "антиутопия", "Б3")));
     }
 
-    @Test(expected = Exception.class)
-    public void ExRemove() {
-        Library library = new Library();
-        library.add(new Book("гордость и предубеждение", "джейн остин", "роман", "A2"));
-        library.add(new Book("мы", "замятин", "антиутопия", "Б3"));
-        library.remove(new Book("гордость", "джейн остин", "роман", "A2"));
-        library.remove(new Book("vs", "замятин", "антиутопия", "Б3"));
-    }
 
     @Test
     public void changes() {
@@ -41,15 +37,11 @@ public class LibraryTest {
         library.changes(new Book("время жить и время умирать", "эрих мария", "роман", "T7"),
                 null, "ремарк", "антивоенный роман");
         assertTrue(library.contains(new Book("время жить и время умирать", "ремарк", "антивоенный роман", "T7")));
+
+        assertFalse(library.changes(new Book("десять негритят", "агата кристи", "детектив", "T7"),
+                null, "ремарк", "антивоенный роман"));
     }
 
-    @Test(expected = Exception.class)
-    public void ExChanges() {
-        Library library = new Library();
-        library.add(new Book("время жить и время умирать", "эрих мария", "роман", "T7"));
-        library.changes(new Book("десять негритят", "агата кристи", "детектив", "T7"),
-                null, "ремарк", "антивоенный роман");
-    }
 
     @Test
     public void shift() {
@@ -57,13 +49,8 @@ public class LibraryTest {
         library.add(new Book("собор парижской богоматери", "гюго", "роман", "89"));
         library.shift(new Book("собор парижской богоматери", "гюго", "роман", "89"), "D8");
         assertTrue(library.contains(new Book("собор парижской богоматери", "гюго", "роман", "D8")));
-    }
-
-    @Test(expected = Exception.class)
-    public void ExShift() {
-        Library library = new Library();
-        library.add(new Book("собор парижской богоматери", "гюго", "роман", "89"));
-        library.shift(new Book("собор парижской богоматери", "виктор", "роман", "89"), "D8");
+        
+        assertFalse(library.shift(new Book("собор парижской богоматери", "виктор", "роман", "89"), "K8"));
     }
 
     @Test
@@ -74,21 +61,14 @@ public class LibraryTest {
         library.add(new Book("записки юного врача", "булгаков", "цикл рассказов", "G1"));
         library.add(new Book("451", "брэдбери", "анти", "F2"));
         library.add(new Book("градус", "брэдбери", "утопия", "F2"));
-        HashSet<Book> books = library.search(null, "брэдбери", null, "F2");
-        HashSet<Book> ideal = new HashSet<Book>();
+        Set<Book> books = library.search(null, "брэдбери", null, "F2");
+        Set<Book> ideal = new HashSet<Book>();
         ideal.add(new Book("451 градус по фаренгейту", "брэдбери", "антиутопия", "F2"));
         ideal.add(new Book("451", "брэдбери", "анти", "F2"));
         ideal.add(new Book("градус", "брэдбери", "утопия", "F2"));
         assertEquals(ideal, books);
-    }
 
-    @Test(expected = Exception.class)
-    public void ExSearch() {
-        Library library = new Library();
-        library.add(new Book("1984", "оруэлл", "роман", "Y0"));
-        library.add(new Book("451 градус по фаренгейту", "брэдбери", "антиутопия", "F2"));
-        library.add(new Book("записки юного врача", "булгаков", "цикл рассказов", "G1"));
-        HashSet<Book> firstBooks = library.search(null, "брэдбери", null, "F1");
-        HashSet<Book> secondBooks = library.search("1984", null, "антиутопия", null);
+        assertTrue(library.search(null, "брэдбери", null, "F1").isEmpty());
+        assertTrue(library.search("1984", null, "антиутопия", null).isEmpty());
     }
 }
